856f6a4af6dc0daa1fbf8d88508ce83444094d2b
kevkar <58485484+kevkar@users.noreply.github.com>
2020-08-25 15:12:14 -0400

    fixed while loop for theTweet

--- Testing.py
+++ Testing.py
@@ -60,13 +60,14 @@ def doProgram(api,movieDict,eventDict):
     while True:
         print("Type y to generate new tweet, t to tweet, q to quit")
         randomMovie = ''
+        theTweet = '';
         x = input()
         if x == 'y':
             randomMovie = randomMovieTitle(movieDict)
             theTweet = getTweet(randomMovie, movieDict, eventDict)
             print(theTweet)
         elif x == 't':
-            if (randomMovie == ''):
+            if (randomMovie == '' || theTweet == ''):
                 print("You have to generate a tweet first!")
                 continue
             tweet(api, theTweet)
